36f00ca6f416c0152804a7598c87f0c37d22d312
Elif Naz Tosun <elfnaztosun@icloud.com>
2026-06-14 07:54:03 +0300

    first commit

---
+++ mit.py
@@ -0,0 +1,57 @@
+# x = int(input("how old are you? "))
+# while x==20:
+#    print("You are 20? It's ok bbg, no one has to know")
+#    x = (input("how old are you again? "))
+# print("well, there goes my joke")
+
+# n = int(input("Enter a non-negative integer: "))
+# while n > 0:
+#     print('x')
+#     n = n-1  # the same as n -= 1
+
+# num = int(input("tell me how many times you want to see me: "))
+# while num > 0:
+#     print("me "*2)
+#     num = num - 1
+# print("hehe you saw even more of me")
+
+# n = 1
+# where = input("Go left or right? ")
+# while where == "right" and n>0:
+#     where = input("Go left or right? ")
+#     n = n-1
+#     if n <= 0 :
+#         print(":,(")
+#         n = 2
+# print("You got out!")
+
+# x = int(input("Which number do you want to find the factorial of? "))
+# i=1
+# factorial=1
+# while i <= x:
+#     factorial *= i
+#     i += 1
+# print(f"{x} factorial is {factorial}")
+
+# for i in range(1,4,1):
+#     print(i)
+# for j in range(1,4,2):
+#     print(j*2)
+# for me in range(4,0,-1):
+#     print("$"*me)
+# mysum=0
+# for i in range(10):
+#     mysum += i
+#     print(mysum) 
+#     if mysum>30: print(mysum,"!")
+
+# mysum=0
+# start=3
+# end=5
+# for i in range(start,end+1):
+#     mysum+=i
+# print(mysum)
+
+#finger exercises for lecture 3
+for N in range (10):
+    print("hello world")
